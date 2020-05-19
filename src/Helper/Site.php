d26b90f51c65aa1ac0041635b8f1aa3f0ee56a29
Dimitri BOUTEILLE <34821762+dimitriBouteille@users.noreply.github.com>
2020-05-19 23:38:56 +0200

    Update Helper/Site and Helper/WcUrl

--- src/Helper/Site.php
+++ src/Helper/Site.php
@@ -2,9 +2,25 @@
 
 namespace Dbout\WpCore\Helper;
 
+use http\Encoding\Stream;
+
 /**
  * Class Site
  * @package Dbout\WpCore\Helper
+ *
+ * @method string name();
+ * @method string description();
+ * @method string url();
+ * @method string siteUrl();
+ * @method string language();
+ * @method string charset();
+ * @method string adminEmail();
+ * @method string pingbackUrl();
+ * @method string atomUrl();
+ * @method string rdfUrl();
+ * @method string rssUrl();
+ * @method string wpVersion();
+ * @method Theme theme();
  *
  * @author      Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
  * @link        https://github.com/dimitriBouteille Github
@@ -14,152 +30,126 @@ class Site
 {
 
     /**
-     * Get site name
-     *
-     * @return string
+     * @var string
      */
-    public static function name(): string
+    public $name;
+
+    /**
+     * @var string
+     */
+    public $description;
+
+    /**
+     * @var string
+     */
+    public $url;
+
+    /**
+     * @var string
+     */
+    public $siteUrl;
+
+    /**
+     * @var string
+     */
+    public $language;
+
+    /**
+     * @var string
+     */
+    public $charset;
+
+    /**
+     * @var string|null
+     */
+    public $adminEmail;
+
+    /**
+     * @var string|null
+     */
+    public $pingbackUrl;
+
+    /**
+     * @var string
+     */
+    public $atomUrl;
+
+    /**
+     * @var string
+     */
+    public $rdfUrl;
+
+    /**
+     * @var string
+     */
+    public $rssUrl;
+
+    /**
+     * @var string
+     */
+    public $rss2Url;
+
+    /**
+     * @var string
+     */
+    public $wpVersion;
+
+    /**
+     * @var Theme
+     */
+    public $theme;
+
+    /**
+     * Site constructor.
+     */
+    public function __construct()
     {
-        return self::get('name');
+        $this->init();
     }
 
     /**
-     * Get site description
-     *
-     * @return string
+     * Init class
      */
-    public static function description(): string
+    protected function init(): void
     {
-        return self::get('description');
+        $this->name = $this->getInfo('name');
+        $this->description = $this->getInfo('description');
+        $this->url = $this->getInfo('url');
+        $this->siteUrl = site_url();
+        $this->language = $this->getInfo('language');
+        $this->charset = $this->getInfo('charset');
+        $this->adminEmail = $this->getInfo('admin_email');
+        $this->wpVersion = $this->getInfo('version');
+        $this->pingbackUrl = $this->getInfo('pingback_url');
+        $this->atomUrl = $this->getInfo('atom_url');
+        $this->rdfUrl = $this->getInfo('rdf_url');
+        $this->rssUrl = $this->getInfo('rss_url');
+        $this->rss2Url = $this->getInfo('rss2_url');
+
+        $this->theme = Theme::getInstance();
     }
 
     /**
-     * Get site url
-     *
-     * @return string
+     * @param string $key
+     * @return string|void
      */
-    public static function url(): string
+    protected function getInfo(string $key)
     {
-        return self::get('url');
+        return get_bloginfo($key);
     }
 
     /**
-     * Get current language code
-     *
-     * @return string|null
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
      */
-    public static function language(): ?string
+    public function __call($name, $arguments)
     {
-        return self::get('language');
-    }
+        if(property_exists($this, $name)) {
+            return $this->{$name};
+        }
 
-    /**
-     * Get encoding
-     *
-     * @return string|null
-     */
-    public static function charset(): ?string
-    {
-        return self::get('charset');
+        throw new \Exception(sprintf("Property %s not found", $name));
     }
-
-    /**
-     * Get admin email
-     *
-     * @return string|null
-     */
-    public static function adminEmail(): ?string
-    {
-        return self::get('admin_email');
-    }
-
-    /**
-     * Get current Wordpress version
-     *
-     * @return string|null
-     */
-    public static function currentWpVersion()
-    {
-        return self::get('version');
-    }
-
-    /**
-     * Get pingback XML-RPC file URL
-     *
-     * @return string|null
-     */
-    public static function pingBackUrl()
-    {
-        return self::get('pingback_url');
-    }
-
-    /**
-     * Get Atom feed url
-     *
-     * @return string|null
-     */
-    public static function atomUrl()
-    {
-        return self::get('atom_url');
-    }
-
-    /**
-     * Get RDF/RSS 1.0 feed URL
-     *
-     * @return string|null
-     */
-    public static function rdfUrl()
-    {
-        return self::get('rdf_url');
-    }
-
-    /**
-     * Get RSS 0.92 feed URL
-     *
-     * @return string|null
-     */
-    public static function rssUrl()
-    {
-        return self::get('rss_url');
-    }
-
-    /**
-     * Get RSS 2.0 feed URL
-     *
-     * @return string|null
-     */
-    public static function rss2Url()
-    {
-        return self::get('rss2_url');
-    }
-
-    /**
-     * Get comments Atom feed URL
-     *
-     * @return string|null
-     */
-    public static function commentsAtomUrl()
-    {
-        return self::get('comments_atom_url');
-    }
-
-    /**
-     * Get comments RSS 2.0 feed URL
-     *
-     * @return string|null
-     */
-    public static function commentsRss2Url()
-    {
-        return self::get('comments_rss2_url');
-    }
-
-    /**
-     * @param string $optionName
-     * @return string|null
-     */
-    protected static function get(string $optionName): ?string
-    {
-        return get_bloginfo($optionName);
-    }
-
 }
--- src/Helper/WcUrl.php
+++ src/Helper/WcUrl.php
@@ -7,6 +7,17 @@ namespace Dbout\WpCore\Helper;
  * @package Dbout\WpCore\Helper
  *
  * https://wpcrumbs.com/how-to-get-woocommerce-page-urls-in-woocommerce-3-x/
+ * @method string account();
+ * @method string cart();
+ * @method string shop();
+ * @method string checkout();
+ * @method string accountOrders();
+ * @method string accountDownloads();
+ * @method string accountEdit();
+ * @method string accountAddress();
+ * @method string accountPaymentMethods();
+ * @method string accountLostPassword();
+ * @method string accountLogout();
  *
  * @author      Dimitri BOUTEILLE <bonjour@dimitri-bouteille.fr>
  * @link        https://github.com/dimitriBouteille Github
@@ -16,63 +27,68 @@ class WcUrl
 {
 
     /**
-     * Get account url
-     *
-     * @return string|null
+     * @var string[]
      */
-    public static function myAccountUrl(): ?string
+    protected $urls = [];
+
+    /**
+     * WcUrl constructor.
+     */
+    public function __construct()
     {
-        return static::getUrl('myaccount');
+        $this->init();
     }
 
     /**
-     * Get account endpoint url
-     *
+     * Init class
+     */
+    protected function init(): void
+    {
+        $this->urls = [
+            'account' => $this->getUrl('myaccount'),
+            'cart' => $this->getUrl('cart'),
+            'shop' => $this->getUrl('shop'),
+            'checkout' => $this->getUrl('checkout'),
+            'accountOrders' => $this->getAccountEndpoint('orders'),
+            'accountDownloads' => $this->getAccountEndpoint('downloads'),
+            'accountEdit' => $this->getAccountEndpoint('edit-account'),
+            'accountAddress' => $this->getAccountEndpoint('edit-address'),
+            'accountPaymentMethods' => $this->getAccountEndpoint('payment-methods'),
+            'accountLostPassword' => $this->getAccountEndpoint('lost-password'),
+            'accountLogout' => $this->getAccountEndpoint('customer-logout'),
+        ];
+    }
+
+    /**
      * @param string $endpoint
      * @return string|null
      */
-    public static function accountEndpoint(string $endpoint): ?string
+    public function getAccountEndpoint(string $endpoint): ?string
     {
         return wc_get_account_endpoint_url($endpoint);
-    }
-
-    /***
-     * Get shop url
-     *
-     * @return string|null
-     */
-    public static function shopUrl(): ?string
-    {
-        return static::getUrl('shop');
-    }
-
-    /**
-     * Get cart url
-     *
-     * @return string|null
-     */
-    public static function cartUrl(): ?string
-    {
-       return static::getUrl('cart');
-    }
-
-    /**
-     * Get checkout url
-     *
-     * @return string|null
-     */
-    public static function checkoutUrl(): ?string
-    {
-        return static::getUrl('checkout');
     }
 
     /**
      * @param string $name
      * @return string|null
      */
-    protected static function getUrl(string $name): ?string
+    protected function getUrl(string $name): ?string
     {
         return wc_get_page_permalink($name);
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws \Exception
+     */
+    public function __call($name, $arguments)
+    {
+        if(key_exists($name, $this->urls)) {
+            return $this->urls[$name];
+        }
+
+        throw new \Exception(sprintf("The url %s not found", $name));
+    }
 }
