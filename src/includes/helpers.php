48ffe3861f0f4012d8e299ac8b92c81936dbd49f
Dimitri BOUTEILLE <34821762+dimitriBouteille@users.noreply.github.com>
2020-08-02 17:15:50 +0200

    Update helpers.php

--- src/includes/helpers.php
+++ src/includes/helpers.php
@@ -4,7 +4,7 @@
  * @param string $path
  * @return string
  */
-function theme_url(string $path): string
+function _theme_url(string $path): string
 {
     return trim(get_template_directory_uri(), '/') . '/' . ltrim($path, '/');
 }
@@ -15,7 +15,18 @@ function theme_url(string $path): string
  * @param int|null $blogId
  * @return string
  */
-function url(string $path, ?string $schema = null, ?int $blogId = null): string
+function _url(string $path, ?string $schema = null, ?int $blogId = null): string
+{
+    return get_home_url($blogId, $path, $schema);
+}
+
+/**
+ * @param string $path
+ * @param string|null $schema
+ * @param int|null $blogId
+ * @return string
+ */
+function _site_url(string $path, ?string $schema = null, ?int $blogId = null): string
 {
     return get_site_url($blogId, $path, $schema);
 }
@@ -23,7 +34,7 @@ function url(string $path, ?string $schema = null, ?int $blogId = null): string
 /**
  * @return string
  */
-function wp_ajax_url(): string
+function _wp_ajax_url(): string
 {
     return admin_url('admin-ajax.php');
 }
